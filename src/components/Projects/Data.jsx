689d77249403ef77ba7605000ed97fc127c34521
Robertino <robertinobarbuto@gmail.com>
2025-12-20 21:45:09 -0300

    bujia name change

--- src/components/Projects/Data.jsx
+++ src/components/Projects/Data.jsx
@@ -1,7 +1,7 @@
 import Image1 from '../../assets/PizzaBlock.png'
 import Image2 from '../../assets/xplora.jpg'
 import Image3 from '../../assets/CoffeU.png'
-import Image4 from '../../assets/laBujiaLoca.png'
+import Image4 from '../../assets/labujialoca.png'
 
 export const Data = [
     {
